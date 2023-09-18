e1ad70cb6349e204824c8a815af147fca5f13163
ymiyamoto63 <miyamooo63@gmail.com>
2023-09-18 17:40:38 +0900

    テストケース追加

--- src/lib/InputResult.test.ts
+++ src/lib/InputResult.test.ts
@@ -34,3 +34,37 @@ describe('calculatePoint', () => {
 		expect(actual).toEqual(expected);
 	});
 });
+
+describe('calculatePoint', () => {
+	it('normal pattern2', () => {
+		const playerPoints = [
+			{ player: 'player1', player_point: 19200 },
+			{ player: 'player2', player_point: -2800 },
+			{ player: 'player3', player_point: 26500 },
+			{ player: 'player4', player_point: 57100 }
+		];
+
+		const registeredGathering = {
+			id: crypto.randomUUID(),
+			created_at: new Date(),
+			genten: 25000,
+			kaeshi: 30000,
+			uma1: 20,
+			uma2: 10,
+			player1: 'player1',
+			player2: 'player2',
+			player3: 'player3',
+			player4: 'player4'
+		};
+
+		const actual = calculatePoint(playerPoints, registeredGathering);
+
+		const expected = [
+			{ player: 'player4', player_point: 68 },
+			{ player: 'player3', player_point: 6 },
+			{ player: 'player1', player_point: -21 },
+			{ player: 'player2', player_point: -53 }
+		];
+		expect(actual).toEqual(expected);
+	});
+});
